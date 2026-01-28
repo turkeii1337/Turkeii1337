78787951e6840c852f96a233712abed5b02ba95a
Türkeii <bhektz@gmail.com>
2026-01-28 01:37:36 +0100

    Delete Türkeii_Leftpeak directory

--- Türkeii_Leftpeak/config.lua
+++
@@ -1,9 +0,0 @@
-Config = {
-    easeTime = 100, -- Transition time in milliseconds
-    leftRange = 1.0, -- from 0.5 to 1.20
-    keymapping = {
-        name = "Türkeii_Leftpeak",
-        description = "Toggle Left Shoulder",
-        key = "G"
-    }
-}
